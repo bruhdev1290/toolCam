fb6006f1ea5cd40aafce1b2f105eb52f89ee28e4
tim000x3 <timeister@mac.com>
2026-02-08 04:10:20 -0500

    New logo, who dis?

--- vite.config.js
+++ vite.config.js
@@ -19,9 +19,14 @@ export default defineConfig({
         theme_color: '#000000',
         icons: [
           {
-            src: '/icon.svg',
+            src: '/icon-192.png',
             sizes: '192x192',
-            type: 'image/svg+xml',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
           },
         ],
         categories: ['photo', 'utilities'],
@@ -35,7 +40,7 @@ export default defineConfig({
         },
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,svg}'],
+        globPatterns: ['**/*.{js,css,html,svg,png}'],
       },
     }),
   ],
